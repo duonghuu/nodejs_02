d3fd9f81ca7d89dc6385e881a0ce1d8008f65a23
duonghuu <huuduong9@gmail.com>
2025-03-12 13:56:45 +0700

    feat: connection pool

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
 const mysql = require("mysql2");
 
 // Create the connection to database
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3307,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
 
 module.exports = connection;

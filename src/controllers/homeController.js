79ca18c4687f833ef74ac4e93919668adfb75a5a
duonghuu <huuduong9@gmail.com>
2025-05-01 22:38:14 +0700

    feat: #62 display list user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,7 +2,7 @@ const { getAllUsers, getUserById, updateUserById, deleteUserById } = require("..
 const connection = require("../config/database");
 const User = require("../models/user");
 const getHomepage = async (req, res) => {
-    let results = [];
+    let results = await User.find({});
     return res.render("home", {
         listUsers: results
     });

71ff96107663baecce3ecc252b8c5a84dd7692f1
duonghuu <huuduong9@gmail.com>
2025-05-02 11:55:14 +0700

    feat: #81 model customer

---
+++ src/models/customer.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    address: String,
+    phone: String,
+    email: String,
+    image: String,
+    description: String
+}, {timestamps: true});
+const Customer = mongoose.model('customer', customerSchema);
+module.exports = Customer;

777ad6f0ba47c4fb34ac696e996c4bc97c02424c
duonghuu <huuduong9@gmail.com>
2025-05-09 10:00:56 +0700

    feat: #104 mongodb driver connect

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 const apiRoutes = require('./routes/api');
 require('dotenv').config()
+const { MongoClient } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT
@@ -25,7 +26,18 @@ app.use('/v1/api', apiRoutes);
 
 (async () => {
   try {
-    await connection()
+    // await connection();
+    // Connection URL
+    const url = process.env.DB_HOST_WITH_DRIVER;
+    const client = new MongoClient(url);
+    const dbName = process.env.DB_NAME;
+
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('documents');
+
     app.listen(port, process.env.HOST_NAME, () => {
       console.log(`Backend app listening on port ${port}`)
     })

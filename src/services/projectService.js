d8dc7a3092ea76924d5cf46b8f467ea72e9c99c3
duonghuu <huuduong9@gmail.com>
2025-05-10 00:52:04 +0700

    feat: #115 add users into project

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -6,6 +6,14 @@ const createEmptyCustomerService = async (objData) => {
             let result = await Project.create(objData);
             return result;
         }
+        if (objData.type === "ADD-USERS") {
+            let myProject = await Project.findById(objData.projectId).exec();
+            for (let i = 0; i < objData.usersArr.length; i++) {
+                myProject.usersInfo.push(objData.usersArr[i]);
+            }
+            let result = await myProject.save();
+            return result;
+        }
         return null
     } catch (error) {
         return null;
